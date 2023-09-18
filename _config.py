44dd742299106c99ab419443d7f1293faccf4801
JdM <julien@rmbi.ch>
2023-09-18 19:12:50 +0200

    Fixed data sql ID auto increment

--- _config.py
+++ _config.py
@@ -2,13 +2,12 @@ MAX_PAGE_TEXT_LENGTH = 500*1000
 
 SQL_CREATE_TABLE_QUERY = f"""
 CREATE TABLE IF NOT EXISTS pages (
-    ID int,
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
     URL varchar(2000),
     title varchar(255),
     description varchar(255),
     text varchar({MAX_PAGE_TEXT_LENGTH}),
     domain varchar(255),
-    IP varchar(255),
-    PRIMARY KEY (ID)
+    IP varchar(255)
 );
 """
