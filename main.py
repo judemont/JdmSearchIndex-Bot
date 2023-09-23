76fe146aa0b594eb0085fc72f42c36fc0593f850
JdM <julien@rmbi.ch>
2023-09-23 21:51:42 +0200

    fixed variable case issue

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from src.page_scraper import PageScraper
-from src.data_manager import DataManger
+from src.data_manager import DataManager
 import _config as conf
 
-def handlePages(url, dataManager):
-    # try:
+def handle_pages(url, data_manager):
+    try:
         page_scrape = PageScraper(url, conf.HEADERS, conf.MAX_PAGE_TEXT_LENGTH).scrape()
-        
+
         print(page_scrape.get_page_data())
-        dataManager.save_page_data(page_scrape.get_page_data())
+        data_manager.save_page_data(page_scrape.get_page_data())
 
         page_URLs = page_scrape.get_outlinks()
         for pageURL in page_URLs:
             print(pageURL)
-            if not dataManager.is_link_visited(pageURL):
-                handlePages(pageURL, DataManger)
-    # except RecursionError as err:
-    #     print(err)
-    #     exit();
-    # except Exception as err:
-    #     print(err)
+            if not data_manager.is_link_visited(pageURL):
+                handle_pages(pageURL, data_manager)
+    except RecursionError as err:
+        print(err)
+        exit()
+    except Exception as err:
+        print(err)
 
-dataManager = DataManger(conf.API_BASE_URL)
+data_manager = DataManager(conf.API_BASE_URL)
 
 url_to_visit = conf.BASE_URL
 
-handlePages(url_to_visit, dataManager)
+handle_pages(url_to_visit, data_manager)
--- src/data_manager.py
+++ src/data_manager.py
@@ -2,7 +2,7 @@ from src.page_scraper import PageData
 import requests
 import json
 
-class DataManger:
+class DataManager:
     def __init__(self, API_BASE_URL: str):
         self.API_BASE_URL = API_BASE_URL
 
@@ -10,8 +10,8 @@ class DataManger:
         postData = {"url": pdata.url, "title": pdata.title, "text": pdata.text, "domain": pdata.domain, "ip": pdata.ip}
         postUrl = self.API_BASE_URL + "save_page_data.php"
         responseJson = requests.post(postUrl, json = postData)
-        return
-
+        return False
+    
     def is_link_visited(self, url:str):
         postData = {"link": url}
         postUrl = self.API_BASE_URL + "save_page_data.php"
